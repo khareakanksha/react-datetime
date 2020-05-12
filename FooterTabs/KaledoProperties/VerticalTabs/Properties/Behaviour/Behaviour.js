5105115f6c89797390ea1b63fedaa805e8c1896a
khareakanksha <khare.akanksha101@gmail.com>
2020-05-12 23:48:44 +0530

    1 file modified, 2 added, 4 deleted

---
+++ FooterTabs/KaledoProperties/VerticalTabs/Properties/Behaviour/Behaviour.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+// import "./Behavoiur.css";
+
+class Beh extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: [
+        { id: 1, date: "2014-04-18", total: 121.0, name: "A", points: 5 },
+        { id: 2, date: "2014-04-21", total: 121.0, name: "B", points: 10 },
+        { id: 3, date: "2014-08-09", total: 121.0, name: "C", points: 15 },
+        { id: 4, date: "2014-04-24", total: 121.0, name: "D", points: 20 },
+        { id: 5, date: "2014-04-26", total: 121.0, name: "E", points: 25 }
+      ],
+      expandedRows: []
+    };
+  }
+
+  handleRowClick(rowId) {
+    const currentExpandedRows = this.state.expandedRows;
+    const isRowCurrentlyExpanded = currentExpandedRows.includes(rowId);
+
+    const newExpandedRows = isRowCurrentlyExpanded
+      ? currentExpandedRows.filter(id => id !== rowId)
+      : currentExpandedRows.concat(rowId);
+
+    this.setState({ expandedRows: newExpandedRows });
+  }
+
+  renderItem(item) {
+    const clickCallback = () => this.handleRowClick(item.id);
+    const itemRows = [
+      <tr onClick={clickCallback} key={"row-data-" + item.id}>
+        <td>{item.date}</td>
+        <td>{item.total}</td>
+      </tr>
+    ];
+
+    if (this.state.expandedRows.includes(item.id)) {
+      itemRows.push(
+        <tr key={"row-expanded-" + item.id}>
+          <td>{item.name}</td>
+          <td>{item.points}</td>
+        </tr>
+      );
+    }
+
+    return itemRows;
+  }
+
+  render() {
+    let allItemRows = [];
+
+    this.state.data.forEach(item => {
+      const perItemRows = this.renderItem(item);
+      allItemRows = allItemRows.concat(perItemRows);
+    });
+
+    return (
+      <table id="data">
+        <thead>
+          <tr>
+            <th>Property</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>{allItemRows}</tbody>
+      </table>
+    );
+  }
+}
+
+export default Beh;
--- FooterTabs/KaledoProperties/VerticalTabs/Properties/Properties.js
+++ FooterTabs/KaledoProperties/VerticalTabs/Properties/Properties.js
@@ -1,57 +1,12 @@
 import React, { Component } from "react";
-import "./Properties.css";
-import ToggleBox from "./Behaviour/ToggleBox";
-import Vehicles from "./Behaviour/Vehicles";
 import Beh from "./Behaviour/Beh";
 
-
 class Properties extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      students: [
-        { Property: "Wasif", Value: 21 },
-        { Property: "Ali", Value: 19 },
-        { Property: "Saad", Value: 16 },
-        { Property: "Asad", Value: 25 }
-      ]
-    };
-
-  }
-
-  renderTableHeader() {
-    let header = Object.keys(this.state.students[0]);
-    return header.map((key, index) => {
-      return <th key={index}>{key}</th>;
-    });
-  }
-
-  renderTableData() {
-    return this.state.students.map((student, index) => {
-      const { Property, Value } = student;
-      return (
-        <tr>
-          <td>{Property}</td>
-          <td>{Value}</td>
-        </tr>
-      );
-    });
-  }
-
   render() {
     return (
       <form>
         <div>
-          <Beh/>
-          <table id="students">
-            <tbody>
-              <tr>{this.renderTableHeader()}</tr>
-              <ToggleBox title="Show Vehicles">
-            <Vehicles />
-          </ToggleBox>
-              {this.renderTableData()}
-            </tbody>
-          </table>
+          <Beh />
         </div>
       </form>
     );
